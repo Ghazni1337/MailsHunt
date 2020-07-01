928d17cc9b3928d3994cce8b39828b681609165e
Kesara Wimal <kesarawimal9@gmail.com>
2020-07-02 00:50:05 +0530

    Revert "ad remove temporary"
    This reverts commit 164279d9

--- resources/views/ads/ad_bottom.blade.php
+++ resources/views/ads/ad_bottom.blade.php
@@ -1,29 +1,29 @@
-{{--<div class="row text-center ad-row">--}}
-{{--    <div class="col-md-12 d-none d-md-block ad-board">--}}
-{{--        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
-{{--        <!-- mailshunt bottom box -->--}}
-{{--        <ins class="adsbygoogle"--}}
-{{--             style="display:block"--}}
-{{--             data-ad-client="ca-pub-1150347576598742"--}}
-{{--             data-ad-slot="2223157984"--}}
-{{--             data-ad-format="horizontal"--}}
-{{--             data-full-width-responsive="true"></ins>--}}
-{{--        <script>--}}
-{{--            (adsbygoogle = window.adsbygoogle || []).push({});--}}
-{{--        </script>--}}
-{{--    </div>--}}
+<div class="row text-center ad-row">
+    <div class="col-md-12 d-none d-md-block ad-board">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- mailshunt bottom box -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-1150347576598742"
+             data-ad-slot="2223157984"
+             data-ad-format="horizontal"
+             data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
 
-{{--    <div class="col-sm-12 d-md-none ad-box">--}}
-{{--        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
-{{--        <!-- mailshunt bottom box -->--}}
-{{--        <ins class="adsbygoogle"--}}
-{{--             style="display:block"--}}
-{{--             data-ad-client="ca-pub-1150347576598742"--}}
-{{--             data-ad-slot="2223157984"--}}
-{{--             data-ad-format="auto"--}}
-{{--             data-full-width-responsive="true"></ins>--}}
-{{--        <script>--}}
-{{--            (adsbygoogle = window.adsbygoogle || []).push({});--}}
-{{--        </script>--}}
-{{--    </div>--}}
-{{--</div>--}}
+    <div class="col-sm-12 d-md-none ad-box">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- mailshunt bottom box -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-1150347576598742"
+             data-ad-slot="2223157984"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+</div>

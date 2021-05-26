5d63af73aa22c5651fbcff7f261bfae3530f4186
Ghazni1337 <50219824+Ghazni1337@users.noreply.github.com>
2021-05-27 05:18:33 +0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         // 'mysql' => [
         //     'driver' => 'mysql',
-        //     'host' => env('DB_HOST', '127.0.0.1'),
+        //     'host' => env('DB_HOST', '143.95.238.130'),
         //     'port' => env('DB_PORT', '3306'),
-        //     'database' => env('DB_DATABASE', 'forge'),
-        //     'username' => env('DB_USERNAME', 'forge'),
-        //     'password' => env('DB_PASSWORD', ''),
+        //     'database' => env('DB_DATABASE', 'sun108_new'),
+        //     'username' => env('DB_USERNAME', 'sun108_new'),
+        //     'password' => env('DB_PASSWORD', 'sun108_new'),
         //     'unix_socket' => env('DB_SOCKET', ''),
         //     'charset' => 'utf8mb4',
         //     'collation' => 'utf8mb4_unicode_ci',
